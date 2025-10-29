6e72dcbef044346dfcd3c48094fb6ff43b3939e6
Tomasz Budny <tomaszbudny01@gmail.com>
2025-10-29 20:35:49 +0100

    add contracts project

---
+++ HarvestHub.Services.CultivationHistory.Contracts/CultivationHistoryGrpcService.cs
@@ -0,0 +1,19 @@
+﻿using CultivationHistory;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+
+namespace HarvestHub.Services.CultivationHistory.Contracts
+{
+    public class CultivationHistoryGrpcService : CultivationHistoryService.CultivationHistoryServiceBase
+    {
+        public override Task<Empty> CreateCultivationHistory(CreateCultivationHistoryRequest request, ServerCallContext context)
+        {
+            return base.CreateCultivationHistory(request, context);
+        }
+
+        public override Task<Empty> DeleteCultivationHistory(DeleteCultivationHistoryRequest request, ServerCallContext context)
+        {
+            return base.DeleteCultivationHistory(request, context);
+        }
+    }
+}
