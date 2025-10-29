2a52926a1a24e0053fc645ae8f6268e03f33b9ac
Tomasz Budny <tomaszbudny01@gmail.com>
2025-10-29 20:40:13 +0100

    update proto def

--- HarvestHub.Services.CultivationHistory.Contracts/CultivationHistoryGrpcService.cs
+++ HarvestHub.Services.CultivationHistory.Contracts/CultivationHistoryGrpcService.cs
@@ -1,17 +1,16 @@
 ﻿using CultivationHistory;
-using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
 
 namespace HarvestHub.Services.CultivationHistory.Contracts
 {
     public class CultivationHistoryGrpcService : CultivationHistoryService.CultivationHistoryServiceBase
     {
-        public override Task<Empty> CreateCultivationHistory(CreateCultivationHistoryRequest request, ServerCallContext context)
+        public override Task<CreateCultivationHistoryReply> CreateCultivationHistory(CreateCultivationHistoryRequest request, ServerCallContext context)
         {
             return base.CreateCultivationHistory(request, context);
         }
 
-        public override Task<Empty> DeleteCultivationHistory(DeleteCultivationHistoryRequest request, ServerCallContext context)
+        public override Task<DeleteCultivationHistoryReply> DeleteCultivationHistory(DeleteCultivationHistoryRequest request, ServerCallContext context)
         {
             return base.DeleteCultivationHistory(request, context);
         }
